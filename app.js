fdace9725d70e3197d0a1121dd63e93f358f80f5
David Corona <corona_121@hotmail.com>
2023-07-11 21:03:29 +0200

    Conectado a bbdd local

--- app.js
+++ app.js
@@ -1,9 +1,23 @@
 const express = require('express'); 
 const app = express(); // ejecuta el paquete como una funcion y devuelve una app de express
+const mysql = require('mysql2');
+
+var connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "Enero100190!",
+    // database: "DBname"
+});
+  
+connection.connect(function(err) {
+    if (err) throw err;
+    console.log("Conectado a BBDD");
+});
 
 // Middleware
 app.use((req, res, next) => {
-    console.log("Testing Middleware");
+    console.log("In Middleware");
     next();
 });
 
